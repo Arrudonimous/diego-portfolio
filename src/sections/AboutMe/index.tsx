a926c89aa4404c987950d3dab4d187b6cb95be54
Diego Arruda <arrudadiego45@gmail.com>
2022-10-23 17:10:50 -0300

    AboutMe responsive

--- src/sections/AboutMe/index.tsx
+++ src/sections/AboutMe/index.tsx
@@ -3,12 +3,13 @@ import Card from '../../components/Card';
 
 export default function AboutMe() {
   return (
-    <div className="flex flex-col mt-20 items-center justify-center">
-      <img src={ProfilePic} alt="" className="w-80 mb-8 hover:scale-[1.01]" />
+    <div className="flex flex-col mt-20 items-center justify-center md:flex-row md:gap-52 md:mt-36">
+      <img src={ProfilePic} alt="" className="w-80 mb-8 hover:scale-[1.01] md:w-[50rem]" />
       <div className="w-full">
         <Card text="🧐 Sobre mim" />
-        <h1 className="text-white font-extrabold text-2xl mt-16 mb-3">Diego Arruda</h1>
-        <div className="flex flex-col font-medium text-white opacity-50 gap-3">
+        <h1 className="text-white font-extrabold text-2xl mt-16 mb-3 md:hidden">Diego Arruda</h1>
+        <h1 className="text-white font-extrabold text-5xl mt-16 mb-3 hidden md:flex">Diego de Veras Arruda</h1>
+        <div className="flex flex-col font-medium text-lg text-white opacity-50 gap-3 md:mt-5">
           <span className="block">
             👱🏻‍♂️Pode me chamar apenas de Diego! Sempre fui apaixonado por tecnologia e
             suas vertentes, em 2020 entrei no curso de Desenvolvimento de Sistemas,
