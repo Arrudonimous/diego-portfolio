78717f832f3f0dec2ac7474ce968bd4b590e29ac
Diego Arruda <arrudadiego45@gmail.com>
2022-10-23 21:29:32 -0300

    Recomendations responsive

--- src/assets/mocks/Recomendations.tsx
+++ src/assets/mocks/Recomendations.tsx
@@ -1,12 +1,14 @@
-import Image1 from '../images/ImageTest.jpg';
+import Mario from '../images/Mario.jpg';
 
 export default [
   {
-    description: `O Washington está sempre estudando e aprendendo, buscando aprimorar aquilo que faz.
-    Esse é sua melhor qualidade. Está sempre em busca de seus objetivos, com foco e organização.
-    Além disso, tecnicamente, no que se propõe a fazer, faz bem feito. Sempre!`,
-    image: Image1,
-    name: 'Romário Lima',
-    role: 'Engenheiro da computação',
+    description: `...Ademais, ele foi um 
+    excelente aluno, sempre agindo com respeito e sendo
+    uma pessoa participativa nas aulas, além disso ele era um aluno
+    pontual e assíduo nas aulas, obtendo um excelente aproveitamento em 
+    todas as avaliações referentes a matéria em que lecionei...!`,
+    image: Mario,
+    name: 'Mario de Jesus',
+    role: 'Analista de sistemas',
   },
 ];
--- src/sections/Recomendations/index.tsx
+++ src/sections/Recomendations/index.tsx
@@ -8,12 +8,13 @@ export default function Recomendations() {
   return (
     <div className="mt-16">
       <Card text="💬 Recomendações" />
-      <h1 className="mt-4 font-extrabold text-white text-2xl">Em </h1>
-      <h1 className="font-extrabold text-white text-2xl">depoimento</h1>
+      <h1 className="mt-4 font-extrabold text-white text-2xl md:hidden">Em </h1>
+      <h1 className="font-extrabold text-white text-2xl md:hidden">depoimento</h1>
+      <h1 className="font-extrabold text-white text-2xl hidden md:flex md:text-5xl md:mt-6">Em depoimento</h1>
 
       <Swiper
         slidesPerView={1}
-        className="w-full mt-6"
+        className="w-full mt-6 md:hidden"
         loop
         navigation
         pagination={{
@@ -22,7 +23,6 @@ export default function Recomendations() {
         modules={[Navigation, Pagination]}
         spaceBetween={30}
       >
-
         {RecomendationsMock.map((item) => (
           <SwiperSlide>
             <div className="border-2 rounded-lg border-border p-4 flex flex-col">
@@ -41,6 +41,23 @@ export default function Recomendations() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="hidden md:flex md: flex-row md:gap-10">
+        {RecomendationsMock.map((item) => (
+          <div className="border-2 rounded-lg border-border p-4 flex flex-col md:w-[30rem] md:mt-16">
+            <MdFormatQuote size={30} color="#D3301C" />
+            <span className="text-white opacity-50 text-sm">
+              {item.description}
+            </span>
+            <div className="flex flex-row font-bold mt-6 gap-2">
+              <img src={item.image} alt="" className="w-12 h-12 rounded-[6.25rem]" />
+              <div className="flex flex-col">
+                <h1 className="text-white">{item.name}</h1>
+                <span className="text-text opacity-50">{item.role}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
     </div>
   );
