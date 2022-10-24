6284c078105b0cf2f3055860159cc9e5592ccb80
Diego Arruda <arrudadiego45@gmail.com>
2022-10-23 21:37:31 -0300

    Contact responsive

--- src/sections/Contact/index.tsx
+++ src/sections/Contact/index.tsx
@@ -2,29 +2,33 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { IoCopy } from 'react-icons/io5';
 import { TbArrowTopBar } from 'react-icons/tb';
 import { Link } from 'react-scroll';
-import Card from '../../components/Card';
+import CardWork from '../../components/CardWork';
 import LinkInstagram from '../../components/LinkInstagram';
 
 export default function Contact() {
   return (
-    <div className="mt-16">
-      <Card text="📬 Contatos" />
-      <h1 className="mt-4 font-extrabold text-white text-2xl">Entre </h1>
-      <h1 className="font-extrabold text-white text-2xl">em contato</h1>
+    <div className="mt-16 md:flex md:items-center md:justify-center md:flex-col">
+      <CardWork text="📬 Contatos" />
+      <h1 className="mt-4 font-extrabold text-white text-2xl md:hidden">Entre </h1>
+      <h1 className="font-extrabold text-white text-2xl md:hidden">em contato</h1>
+      <h1 className="font-extrabold text-white text-2xl md:text-5xl hidden md:flex md:mt-16">Vamos conversar!</h1>
 
-      <div className="w-[11.6rem] mt-6 opacity-50">
-        <LinkInstagram />
+      <div className="md:flex md:flex-row md:items-center md:gap-10 md:mt-16">
+        <div className="w-[11.6rem] mt-6 opacity-50">
+          <LinkInstagram />
+        </div>
+        <div className="md:flex md:items-center md:flex-col">
+          <h1 className="text-text mt-4 mb-1 font-medium md:text-xl">E-mail:</h1>
+          <div className="flex flex-row items-center gap-2 md:flex-col">
+            <span className="text-white opacity-50 md:text-xl">diegoarrudacontato@gmail.com</span>
+            <CopyToClipboard text="diegoarrudacontato@gmail.com">
+              <IoCopy size={20} color="#D3301C" />
+            </CopyToClipboard>
+          </div>
+        </div>
       </div>
 
-      <h1 className="text-text mt-4 mb-1 font-medium">E-mail:</h1>
-      <div className="flex flex-row items-center gap-2">
-        <span className="text-white opacity-50">diegoarrudacontato@gmail.com</span>
-        <CopyToClipboard text="diegoarrudacontato@gmail.com">
-          <IoCopy size={20} color="#D3301C" />
-        </CopyToClipboard>
-      </div>
-
-      <Link to="Header" smooth offset={-128} spy className="flex flex-row items-center text-text font-semibold my-16 hover:cursor-pointer">
+      <Link to="Header" smooth offset={-128} spy className="flex flex-row items-center text-text font-semibold my-16 hover:cursor-pointer opacity-50">
         <h1>Voltar ao topo</h1>
         <TbArrowTopBar size={20} className="flex" />
       </Link>
