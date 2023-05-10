ad75cded3f60577d4dcb248d3ef7c1d0d3e4a7d5
Diego Arruda <arrudadiego45@gmail.com>
2023-05-10 14:14:34 -0300

    fix

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -4,12 +4,11 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const CACHE = 'pwabuilder-page';
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = 'offline.html';
 
-self.addEventListener('message', (event) => {
+this.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    this.skipWaiting();
   }
 });
 
